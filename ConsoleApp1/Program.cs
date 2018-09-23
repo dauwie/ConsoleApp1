77f4f3484a858ee6bd0969078bd00e1598a072b1
Daevy Dendauw <dauwie@gmail.com>
2018-09-23 16:31:54 +0200

    test !!

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,7 +9,7 @@ namespace HelloWorld
 
             // Keep the console window open in debug mode.
             Console.WriteLine("Press any key to exit.");
-            Console.WriteLine("Press another key tot exit.");
+            Console.WriteLine("Press another key tot exit.!!!");
             Console.ReadKey();
         }
     }
